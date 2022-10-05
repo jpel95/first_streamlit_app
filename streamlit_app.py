03d38134337d6cd1791eb55e8d93b8a3fb8f7096
Jan <115049212+jpel95@users.noreply.github.com>
2022-10-04 20:14:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title ('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard-Boiled')
